07cdc6cc2ec8d9afd4198b69173e210cdd7d86b9
pedrocf01 <pedrocf01@gmail.com>
2024-11-03 19:39:05 -0300

    Update admin.py

--- kiosk_app/admin.py
+++ kiosk_app/admin.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
-from .models import Categoria, Item, Pedido, DetalhePedido
+from .models import Categoria, Item, Ingrediente, TamanhoItem, Pedido, Promocao
 
 admin.site.register(Categoria)
+admin.site.register(Ingrediente)
 admin.site.register(Item)
+admin.site.register(TamanhoItem)
 admin.site.register(Pedido)
-admin.site.register(DetalhePedido)
+admin.site.register(Promocao)
+# admin.site.register(DetalhePedido)
 
 
