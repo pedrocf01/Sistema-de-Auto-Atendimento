dcc8fc4c11c80c24fb62938d057a0b29151fd347
pedrocf01 <pedrocf01@gmail.com>
2024-11-03 19:40:20 -0300

    Update views.py

--- kiosk_app/views.py
+++ kiosk_app/views.py
@@ -59,9 +59,9 @@ def adicionar_ao_carrinho(request):
             detalhe_pedido.quantidade_item += 1
             detalhe_pedido.save()
 
-        return redirect('carrinho')
+        return redirect('kiosk_app:carrinho')
     else:
-        return redirect('cardapio')
+        return redirect('kiosk_app:cardapio')
 
 
 
@@ -92,4 +92,45 @@ def remover_item_carrinho(request, detalhe_pedido_id):
     detalhe_pedido.delete()
     return redirect('carrinho') 
 
-   
+# def add_carrinho(request, item_id):
+#     # Fetch the item being added to the cart
+#     item = get_object_or_404(Item, id=item_id)
+
+#     # Get the cart from the session, or create an empty one if not exists
+#     carrinho = request.session.get('carrinho', {})
+
+#     # Get the quantity from the POST request (default to 1)
+#     qtd= int(request.POST.get('quantidade', 1))
+    
+#     # Check if the item is already in the cart
+#     if str(item_id) in carrinho:
+#         # If the item is already in the cart, update its quantity
+#         carrinho[str(item_id)]['quantidade'] += qtd
+#     else:
+#         # Otherwise, add the item to the cart
+#         carrinho[str(item_id)] = {
+#             'quantidade': qtd,
+#             'preco': str(item.preco_item),  # Save the price in case it changes later
+#             'nome': item.nome_item,
+#         }
+
+#     # Save the updated cart in the session
+#     request.session['carrinho'] = carrinho
+
+#     # Optionally, add a message (e.g., using Django's messages framework) messages.success(request, f'Added {quantity} {item.item_name}(s) to your cart.')
+
+#     # Redirect to another page (e.g., the cart view or the menu)
+#     return redirect('cardapio')  # Adjust the redirect as needed
+                
+
+# def carrinho_view(request):
+#     # Assume cart is stored in session
+#     carrinho = request.session.get('carrinho', {})
+#     itens = Item.objects.filter(id__in=carrinho.keys())
+#     total = sum(item.preco_item * carrinho[str(item.id)] for item in itens)
+#     context = {
+#         'itens': itens,
+#         'carrinho': carrinho,
+#         'total': total,
+#     }
+#     return render(request, 'carrinho.html', context)                
