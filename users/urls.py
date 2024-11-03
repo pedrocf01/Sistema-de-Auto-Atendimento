e0a23f50f49067d8503ff6751c6c131e3bce811b
pedrocf01 <pedrocf01@gmail.com>
2024-11-03 19:43:50 -0300

    Add files via upload

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import registrar_cliente, LoginUsuarioView
+from .views import registrar_cliente, LoginUsuarioView, logout_view
 from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('registrar/', registrar_cliente, name='registrar_cliente'),
     path('login/', LoginUsuarioView.as_view(), name='login'),
-    path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
+    # path('logout/', LogoutView.as_view(template_name='users/logout_confirm.html'), name='logout'),
+    path('logout/', logout_view, name='logout')
 ]
--- users/views.py
+++ users/views.py
@@ -1,7 +1,10 @@
+from django.http import HttpResponseRedirect
+from django.urls import reverse, reverse_lazy
 from django.shortcuts import render, redirect
 from django.contrib.auth import login
 from .forms import RegistroClienteForm, LoginForm
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth import logout
 
 def registrar_cliente(request):
     if request.method == 'POST':
@@ -9,7 +12,7 @@ def registrar_cliente(request):
         if form.is_valid():
             usuario = form.save()
             login(request, usuario)
-            return redirect('home')
+            return redirect('kiosk_app:cardapio')
     else:
         form = RegistroClienteForm()
     return render(request, 'users/registrar_cliente.html', {'form': form})
@@ -18,7 +21,10 @@ def registrar_cliente(request):
 class LoginUsuarioView(LoginView):
     template_name = 'users/login.html'
     authentication_form = LoginForm
+    redirect_authenticated_user = True  # Redireciona se já estiver logado
+    success_url = reverse_lazy('kiosk_app:cardapio')  
 
 
-
-
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('kiosk_app:cardapio'))
