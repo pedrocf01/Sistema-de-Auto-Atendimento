b030391fb85954420cbb3d70f83eb8d6feac9b61
pedrocf01 <pedrocf01@gmail.com>
2024-11-18 13:35:36 -0300

    Add files via upload

--- kiosk_app/templatetags/custom_filters.py
+++ kiosk_app/templatetags/custom_filters.py
@@ -1,1 +1,10 @@
+from django import template
 
+register = template.Library()
+
+@register.filter
+def divide(value, divisor):
+    try:
+        return value // divisor
+    except (ValueError, ZeroDivisionError):
+        return None
